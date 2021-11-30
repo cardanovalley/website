058f62f769b8abdf99a9a63380b6e92242f2b514
Cardano Valley <cardanovalley@gmail.com>
2021-11-30 08:45:32 -0300

    Adding cyber monday promos

--- js/functions.js
+++ js/functions.js
@@ -229,7 +229,7 @@ $window.on('load', function(){
 
   // Timer
   // Set the date we're counting down to
-  var countDownDate = new Date("Nov 30, 2021 00:00:00").getTime();
+  var countDownDate = new Date("Dec 06, 2021 00:00:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function() {
